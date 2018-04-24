3d7c544fcd6370dd10a9aec9833bb8b1bfc5d76d
Jesse Carter <jcarter@carnegietechnologies.com>
2018-04-24 12:04:12 -0400

    Working RPC with direct reply-to

--- quokka/scratchpad.ts
+++ quokka/scratchpad.ts
@@ -1,5 +1,4 @@
 import * as amqplib from 'amqplib';
-
 import { AmqpSession } from '../src/AmqpSession';
 
 const rabbitUri = 'amqp://rabbitmq:rabbitmq@localhost:5672';
@@ -15,25 +14,34 @@ interface BasicResponse {
 
 (async () => {
   const session = new AmqpSession(rabbitUri);
-  
   await session.init();
 
   const exchange = 'main';
 
+  session.messageSubject.subscribe(x => console.log('logger', x));
+
   // FOR QUOKKA
-  await session.channel.deleteExchange(exchange);
+  //await session.channel.deleteExchange(exchange);
 
   // Set up RPC (replier)
   await session.respond<BasicRequest, BasicResponse>(async (req) => {
-    console.log(req);
     return { msg: `Hello ${req.name}` };
   }, { exchange, bindingKey: 'basicrpc' });
 
-  // Send    
+  await session.respondDirect<BasicRequest, BasicResponse>(async (req) => {
+    return { msg: `Hello ${req.name}` };
+  }, { exchange, bindingKey: 'directrpc' }, (msg) => console.log('server', msg));
 
-  await session.publish(exchange, 'basicrpc', { message: 'hello' });
+  await session.requestDirect<{ msg: string }>({ bindingKey: 'directrpc', exchange }, { name: 'randy' }); /* ? */
+  await session.requestDirect<{ msg: string }>({ bindingKey: 'directrpc', exchange }, { name: 'jesse' }); /* ? */
+  await session.requestDirect<{ msg: string }>({ bindingKey: 'directrpc', exchange }, { name: 'james' }); /* ? */
+  await session.requestDirect<{ msg: string }>({ bindingKey: 'directrpc', exchange }, { name: 'mike' }); /* ? */
 
-  let response = await session.request({ bindingKey: 'basicrpc', exchange }, { name: 'jesse' });
-  response
+
+  let promises = [1, 2, 3].map(x => {
+    return session.requestDirect<{ msg: string }>({ bindingKey: 'directrpc', exchange }, { name: `msg${x}` });
+  });
+
+  await Promise.all(promises); /* ? */
 
 })();
--- src/AmqpSession.ts
+++ src/AmqpSession.ts
@@ -1,10 +1,20 @@
 import * as amqplib from 'amqplib';
 import * as uuid from 'uuid';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { Either } from './Domain';
+
+interface CorrelationMessage {
+  correlationId: string;
+  message: {};
+}
+
+const DIRECT_REPLY_QUEUE = 'amq.rabbitmq.reply-to';
 
 export class AmqpSession {
   private uri: string;
   private connection!: amqplib.Connection;
+  public messageSubject = new Subject<CorrelationMessage>();
+  //private directReplyReady: Promise<void>;
   public channel!: amqplib.Channel;
 
   constructor(uri: string) {
@@ -14,6 +24,21 @@ export class AmqpSession {
   async init() {
     this.connection = await amqplib.connect(this.uri);
     this.channel = await this.connection.createChannel();
+  
+    // TODO: Reconsider how we approach initialization logic (ie. proper singleton style)
+    await this.channel.consume(DIRECT_REPLY_QUEUE, async (msg) => {
+      if (msg === null || !msg.properties.correlationId) {
+        //observer.error(new Error('Received a null response'));
+        return;
+      }
+
+      const correlationMessage: CorrelationMessage = {
+        correlationId: msg.properties.correlationId.toString(),
+        message: JSON.parse(msg.content.toString())
+      }
+
+      this.messageSubject.next(correlationMessage);
+    }, { noAck: true });
   }
 
   async consume(
@@ -36,8 +61,8 @@ export class AmqpSession {
     });
   }
 
-  async publish(exchange: string, bindingKey: string, message: object) {
-    await this.channel.publish(exchange, bindingKey, new Buffer(JSON.stringify(message)), {});
+  async publish(exchange: string, bindingKey: string, message: object, options?: amqplib.Options.Publish | undefined) {
+    await this.channel.publish(exchange, bindingKey, new Buffer(JSON.stringify(message)), options);
   }
 
   async respond<T, U extends object>(handler: (msg: T) => Promise<U>, options: IRpcOptions) {
@@ -67,13 +92,39 @@ export class AmqpSession {
     });
   }
 
-  async request(options: IRpcOptions, payload: object, timeout?: number) {
+  async respondDirect<T, U extends object>(handler: (msg: T) => Promise<U>, options: IRpcOptions, logger?: ({}) => void) {
+    await this.channel.assertExchange(options.exchange, 'topic');
+    const rpcQueue = await this.channel.assertQueue('');
+    await this.channel.bindQueue(rpcQueue.queue, options.exchange, options.bindingKey);
+
+    await this.channel.consume(rpcQueue.queue, async (msg) => {
+      if (msg === null) {
+        throw Error('Received null message');
+      }
+
+      if (logger) {
+        logger(msg.content.toString());
+      }
+      
+      const message = JSON.parse(msg.content.toString()) as T;
+      const response = await handler(message);
+
+      await this.publish('', msg.properties.replyTo, response, {
+        correlationId: msg.properties.correlationId
+      });
+
+      this.channel.ack(msg);
+    });
+  }
+
+  async request<T>(options: IRpcOptions, payload: object, timeout?: number): Promise<Either<T, any>> {
     const replyTo = uuid.v4();
-    const responseKey = `${options.bindingKey}.*`;
+    const correlationId = uuid.v4();
+    const responseKey = `${options.bindingKey}.*.*`;
     const responseQueue = await this.channel.assertQueue('');
     await this.channel.bindQueue(responseQueue.queue, options.exchange, responseKey);
     
-    const response = new Observable((observer) => {
+    const response = new Observable<T>((observer) => {
       (async () => {
         await this.channel.consume(responseQueue.queue, async (msg) => {
           if (msg === null) {
@@ -81,17 +132,33 @@ export class AmqpSession {
             return;
           }
 
-          observer.next(msg.content.toString());
+          // TODO: Find something better than casting to T or add the ability to set validation middlewares
+          observer.next(JSON.parse(msg.content.toString()) as T);
           observer.complete();
 
           //await this.channel.deleteQueue(responseQueue.queue);
         });
 
-        await this.publish(options.exchange, options.bindingKey, payload);
+        await this.publish(options.exchange, options.bindingKey, payload, { replyTo, correlationId });
       })();
     });
 
     return response.first().toPromise();
+  }
+
+  async requestDirect<T extends {}>(options: IRpcOptions, payload: object, timeout?: number): Promise<Either<T, any>> {
+    const correlationId = uuid.v4();
+
+    await this.publish(options.exchange, options.bindingKey, payload, { 
+      replyTo: DIRECT_REPLY_QUEUE,
+      correlationId
+    });
+
+    return this.messageSubject
+      .filter(x => x.correlationId === correlationId)
+      .map(x => x.message)
+      .first()
+      .toPromise();
   }
 }
 
